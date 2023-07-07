da64e205199cbef8984baa884ad9d5a172d9ea90
bgallois <benjamin@gallois.cc>
2023-07-07 18:52:09 +0200

    [feat] remove VTK from dependencies

---
+++ opencv.rb
@@ -0,0 +1,165 @@
+class Opencv < Formula
+  desc "Open source computer vision library"
+  homepage "https://opencv.org/"
+  url "https://github.com/opencv/opencv/archive/refs/tags/4.8.0.tar.gz"
+  sha256 "cbf47ecc336d2bff36b0dcd7d6c179a9bb59e805136af6b9670ca944aef889bd"
+  license "Apache-2.0"
+  revision 1
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 arm64_ventura:  "3237e8ad88aa8d96b0a8a46430dc4e0a0b2540817ee23a2c2e71f9e105895e23"
+    sha256 arm64_monterey: "b302e227dee1ce5e4cdbf36debf29be9383623d71c0fd55bfec8acd44bc2bea9"
+    sha256 arm64_big_sur:  "51058522588ab01e5f252dca1669c165e0701c921a59ee1bb473656728ae3218"
+    sha256 ventura:        "05d4f3e1cdf9a2147aeea87d6c20cbebb3c124c189d277881a74f218d1c36a9a"
+    sha256 monterey:       "bfc174bf69aed4fe6780d2481cbf5aba02e136e19935e27ce9bbd55054422736"
+    sha256 big_sur:        "8c96f8e85d2c2558db4f9a1e7ced99b7f2b47f1d24f277bc1d078c12cb8880ef"
+    sha256 x86_64_linux:   "a2fb888226f1041595b19bad9dd39291e44ead0bc04339001c39c93a1f05549c"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "ceres-solver"
+  depends_on "eigen"
+  depends_on "ffmpeg"
+  depends_on "glog"
+  depends_on "harfbuzz"
+  depends_on "jpeg-turbo"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "numpy"
+  depends_on "openblas"
+  depends_on "openexr"
+  depends_on "openjpeg"
+  depends_on "protobuf"
+  depends_on "python@3.11"
+  depends_on "tbb"
+  depends_on "webp"
+
+  uses_from_macos "zlib"
+
+  fails_with gcc: "5" # ffmpeg is compiled with GCC
+
+  resource "contrib" do
+    url "https://github.com/opencv/opencv_contrib/archive/refs/tags/4.8.0.tar.gz"
+    sha256 "b4aef0f25a22edcd7305df830fa926ca304ea9db65de6ccd02f6cfa5f3357dbb"
+  end
+
+  def python3
+    "python3.11"
+  end
+
+  def install
+    resource("contrib").stage buildpath/"opencv_contrib"
+
+    # Avoid Accelerate.framework
+    ENV["OpenBLAS_HOME"] = Formula["openblas"].opt_prefix
+
+    # Reset PYTHONPATH, workaround for https://github.com/Homebrew/homebrew-science/pull/4885
+    ENV.delete("PYTHONPATH")
+
+    # Remove bundled libraries to make sure formula dependencies are used
+    libdirs = %w[ffmpeg libjasper libjpeg libjpeg-turbo libpng libtiff libwebp openexr openjpeg protobuf tbb zlib]
+    libdirs.each { |l| (buildpath/"3rdparty"/l).rmtree }
+
+    args = %W[
+      -DCMAKE_CXX_STANDARD=11
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=
+      -DBUILD_JASPER=OFF
+      -DBUILD_JPEG=OFF
+      -DBUILD_OPENEXR=OFF
+      -DBUILD_OPENJPEG=OFF
+      -DBUILD_PERF_TESTS=OFF
+      -DBUILD_PNG=OFF
+      -DBUILD_PROTOBUF=OFF
+      -DBUILD_TBB=OFF
+      -DBUILD_TESTS=OFF
+      -DBUILD_TIFF=OFF
+      -DBUILD_WEBP=OFF
+      -DBUILD_ZLIB=OFF
+      -DBUILD_opencv_hdf=OFF
+      -DBUILD_opencv_java=OFF
+      -DBUILD_opencv_text=ON
+      -DOPENCV_ENABLE_NONFREE=ON
+      -DOPENCV_EXTRA_MODULES_PATH=#{buildpath}/opencv_contrib/modules
+      -DOPENCV_GENERATE_PKGCONFIG=ON
+      -DPROTOBUF_UPDATE_FILES=ON
+      -DWITH_1394=OFF
+      -DWITH_CUDA=OFF
+      -DWITH_EIGEN=ON
+      -DWITH_FFMPEG=ON
+      -DWITH_GPHOTO2=OFF
+      -DWITH_GSTREAMER=OFF
+      -DWITH_JASPER=OFF
+      -DWITH_OPENEXR=ON
+      -DWITH_OPENGL=OFF
+      -DWITH_QT=OFF
+      -DWITH_TBB=ON
+      -DWITH_VTK=OFF
+      -DBUILD_opencv_python2=OFF
+      -DBUILD_opencv_python3=ON
+      -DPYTHON3_EXECUTABLE=#{which(python3)}
+    ]
+
+    args += [
+      "-DCMAKE_FIND_PACKAGE_PREFER_CONFIG=ON", # https://github.com/protocolbuffers/protobuf/issues/12292
+      "-Dprotobuf_MODULE_COMPATIBLE=ON", # https://github.com/protocolbuffers/protobuf/issues/1931
+    ]
+
+    # Disable precompiled headers and force opencv to use brewed libraries on Linux
+    if OS.linux?
+      args += %W[
+        -DENABLE_PRECOMPILED_HEADERS=OFF
+        -DJPEG_LIBRARY=#{Formula["jpeg-turbo"].opt_lib}/libjpeg.so
+        -DOpenBLAS_LIB=#{Formula["openblas"].opt_lib}/libopenblas.so
+        -DOPENEXR_ILMIMF_LIBRARY=#{Formula["openexr"].opt_lib}/libIlmImf.so
+        -DOPENEXR_ILMTHREAD_LIBRARY=#{Formula["openexr"].opt_lib}/libIlmThread.so
+        -DPNG_LIBRARY=#{Formula["libpng"].opt_lib}/libpng.so
+        -DPROTOBUF_LIBRARY=#{Formula["protobuf"].opt_lib}/libprotobuf.so
+        -DTIFF_LIBRARY=#{Formula["libtiff"].opt_lib}/libtiff.so
+        -DWITH_V4L=OFF
+        -DZLIB_LIBRARY=#{Formula["zlib"].opt_lib}/libz.so
+      ]
+    end
+
+    # Ref: https://github.com/opencv/opencv/wiki/CPU-optimizations-build-options
+    ENV.runtime_cpu_detection
+    if Hardware::CPU.intel? && build.bottle?
+      cpu_baseline = MacOS.version.requires_sse42? ? "SSE4_2" : "SSSE3"
+      args += %W[-DCPU_BASELINE=#{cpu_baseline} -DCPU_BASELINE_REQUIRE=#{cpu_baseline}]
+    end
+
+    system "cmake", "-S", ".", "-B", "build_shared", *args, *std_cmake_args
+    inreplace "build_shared/modules/core/version_string.inc", "#{Superenv.shims_path}/", ""
+    system "cmake", "--build", "build_shared"
+    system "cmake", "--install", "build_shared"
+
+    system "cmake", "-S", ".", "-B", "build_static", *args, *std_cmake_args, "-DBUILD_SHARED_LIBS=OFF"
+    inreplace "build_static/modules/core/version_string.inc", "#{Superenv.shims_path}/", ""
+    system "cmake", "--build", "build_static"
+    lib.install buildpath.glob("build_static/{lib,3rdparty/**}/*.a")
+
+    # Prevent dependents from using fragile Cellar paths
+    inreplace lib/"pkgconfig/opencv#{version.major}.pc", prefix, opt_prefix
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <opencv2/opencv.hpp>
+      #include <iostream>
+      int main() {
+        std::cout << CV_VERSION << std::endl;
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", "test.cpp", "-I#{include}/opencv4", "-o", "test"
+    assert_equal shell_output("./test").strip, version.to_s
+
+    output = shell_output("#{python3} -c 'import cv2; print(cv2.__version__)'")
+    assert_equal version.to_s, output.chomp
+  end
+end
