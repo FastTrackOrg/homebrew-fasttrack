a0ec3ab576a123a049363285f31aa67faf74d4a8
bgallois <benjamin@gallois.cc>
2024-10-11 12:59:09 +0200

    [feat] update fasttrack

--- fasttrack.rb
+++ fasttrack.rb
@@ -1,9 +1,9 @@
 class Fasttrack < Formula
   desc "FastTrack is a cross-platform application designed to track multiple objects in video recording. "
   homepage "https://fasttrack.sh"
-  url "https://github.com/FastTrackOrg/FastTrack/archive/refs/tags/v6.3.3.tar.gz"
-  sha256 "3d516f48d0fe16da5b7ee2b95adf272a700c83d2d177e7d9ac3fcb1458afa750"
-  license " GPL-3.0"
+  url "https://github.com/FastTrackOrg/FastTrack/archive/refs/tags/v6.3.4.tar.gz"
+  sha256 "7ce8ca265178cb689d677b02c76ab77ba18e1a35ad59ca9d14cf11728f8ffad8"
+  license "GPL-3.0"
   revision 1
 
   depends_on "pkg-config" => :build
